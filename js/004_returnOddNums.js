dff572592ec81cffebcd015a4461615330c7face
Kelly Reynolds <ksreynolds90@icloud.com>
2022-05-11 14:39:04 -0500

    updated comments

--- js/004_returnOddNums.js
+++ js/004_returnOddNums.js
@@ -1,6 +1,7 @@
 //Return odd numbers from a list
 
 function odds(values){
-    return values.filter( v => v%2 );//if number is even, filter will remove it
+    return values.filter( v => v%2 );//if number is even, filter will remove it because it is false
+    
   }
   odds(2,3,4,5,6)
