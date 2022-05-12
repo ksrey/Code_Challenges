79425ca7968e044f1345c131aefc7470bd66cf90
Kelly Reynolds <ksreynolds90@icloud.com>
2022-05-11 21:29:42 -0500

    updated comments

--- js/004_returnOddNums.js
+++ js/004_returnOddNums.js
@@ -4,3 +4,4 @@ function odds(values){
     return values.filter( v => v%2 );//if number is even, filter will remove it
   }
   odds(2,3,4,5,6)
+//create a function named odds with the parameters of 'values'; return values with a filter that returns values divisible by 2; false so it will remove even numbers and return the odd numbers from the list.  Call parameters of numbers.
