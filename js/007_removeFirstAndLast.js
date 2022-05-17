9055b7f4dfeb15bc35ef3ab8fdeb318d7f79ea97
Kelly Reynolds <ksreynolds90@icloud.com>
2022-05-17 15:53:21 -0500

    updated challenge

--- js/007_removeFirstAndLast.js
+++ js/007_removeFirstAndLast.js
@@ -1,8 +1,7 @@
 //remove the first and last character from the string
 function removeChar(str) {
         return str.slice(1, -1);
-       
-      }
+       }
       console.log(removeChar('hello'))
 
      
