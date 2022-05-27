af9df6fb67c7e5d0135cf70896ae990381ca4232
Kelly Reynolds <ksreynolds90@icloud.com>
2022-05-27 18:14:27 -0500

    updated comments

---
+++ js/008_convertBoolToString.js
@@ -0,0 +1,6 @@
+//Implement a function which converts the given boolean value into its string representation ('true' or 'false')
+
+function booleanToString(b){
+    return b.toString();
+  }
+  //toString() will return booleen of true or false based on what is passed in
---
+++ js/009_convertStrToNum.js
@@ -0,0 +1,6 @@
+//Convert a string to a number
+
+const stringToNumber = function(str){
+    return Number(str);
+}
+console.log(stringToNumber('1234'))
